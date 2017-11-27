9ae7620251c0b0d0f18b1956b61df3e56552d810
daguiheso <daguiheso@gmail.com>
2017-11-26 20:26:17 -0500

    Solution fixed Routes in serve

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ const path = require('path');
 const app = express();
 app.use(express.static(path.join(__dirname, 'dist')));
 
+app.get('*', (req, res) => {
+	res.sendFile(path.join(__dirname, 'dist/index.html'));
+});
+
 // Start the server
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
-import { RouterModule } from '@angular/router';
+import { RouterModule, Route } from '@angular/router';
 import { MaterialModule } from '../material.module';
 
 import { AdminRoutingModule } from './admin.routing.module';
