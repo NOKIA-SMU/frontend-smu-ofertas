1d022733ed5b64cc579e66ab3d2ced782aa90045
daguiheso <daguiheso@gmail.com>
2018-01-17 23:12:55 -0500

    Add region config

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import { LOCALE_ID } from '@angular/core';
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
@@ -9,5 +10,7 @@ if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
+platformBrowserDynamic().bootstrapModule(AppModule, {
+  providers: [{ provide: LOCALE_ID, useValue: 'es-CO' }]
+})
   .catch(err => console.log(err));
