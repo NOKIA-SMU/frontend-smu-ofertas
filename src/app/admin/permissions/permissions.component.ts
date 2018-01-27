e62c2a3466a826de336959117629cf4f96759519
daguiheso <daguiheso@gmail.com>
2018-01-27 18:57:53 -0500

    Event change tab implemented

--- src/app/admin/permissions/permissions.component.ts
+++ src/app/admin/permissions/permissions.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 import { AdminService } from '../admin.service';
 import { Permission } from '../../models/auth.models';
 import { AppService } from "../../app.service";
+import { MatTabChangeEvent } from '@angular/material';
 
 @Component({
   selector: "app-permissions",
@@ -15,8 +17,14 @@ export class PermissionsComponent implements OnInit {
   listPermissions: Permission[];
   isShowFormUpdatePermission: boolean = false;
   permissionToEdit: Permission;
+  crud: string[] = ['crear', 'leer', 'editar', 'eliminar'];
+  private selectedTab = 0;
 
-  constructor(private adminService: AdminService, private appService: AppService) { }
+  constructor(
+    private adminService: AdminService,
+    private router: Router,
+    private appService: AppService
+  ) { }
 
   ngOnInit() {
     this.adminService.getPermissions().subscribe(permissions => {
@@ -31,28 +39,34 @@ export class PermissionsComponent implements OnInit {
     this.permissionToEdit = permission;
   }
 
+  changeTab(tabChangeEvent: MatTabChangeEvent): void {
+    this.selectedTab = tabChangeEvent.index;
+  }
+
   createPermission(permission: Permission) {
     this.adminService.createPermission(permission)
       .then(res => {
-        this.appService.showSwal('success-message', 'success', 'Operación Exitosa', 'Permiso creado')
+        this.permission = {};
+        this.selectedTab += 1;
+        if (this.selectedTab >= 2) this.selectedTab = 0;
       }, error => {
         this.appService.showSwal('cancel', 'error', 'Operación sin exito', 'Vuelva a intentarlo')
       });
-    }
+  }
 
-    deletePermission(permission: Permission) {
-      this.adminService.deletePermission(permission)
+  deletePermission(permission: Permission) {
+    this.adminService.deletePermission(permission)
       .then(res => {
         this.appService.showSwal('success-message', 'success', 'Operación Exitosa', 'Permiso eliminado')
       }, error => {
         this.appService.showSwal('cancel', 'error', 'Operación sin exito', 'Vuelva a intentarlo')
       });
-    }
+  }
 
-    updatePermission(permission: Permission) {
-      this.adminService.updatePermission(permission)
+  updatePermission(permission: Permission) {
+    this.adminService.updatePermission(permission)
       .then(res => {
-        this.appService.showSwal('success-message', 'success', 'Operación Exitosa', 'Permiso actualizado')
+        this.isShowFormUpdatePermission = !this.isShowFormUpdatePermission;
       }, error => {
         this.appService.showSwal('cancel', 'error', 'Operación sin exito', 'Vuelva a intentarlo')
       });
